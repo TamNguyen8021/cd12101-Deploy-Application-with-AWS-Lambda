49adb6aa3d900f633ddf580bf0cfb449824f7fce
TamNguyen8021 <tamntt8021@gmail.com>
2024-04-04 22:23:21 +0700

    feat: get todo tasks

--- starter/backend/src/lambda/http/getTodos.js
+++ starter/backend/src/lambda/http/getTodos.js
@@ -1,4 +1,39 @@
-export function handler(event) {
-  // TODO: Get all TODO items for a current user
-  return undefined
+import { createLogger } from '../../utils/logger.mjs'
+import { getUserId } from '../utils.mjs'
+import { getTodoTasks } from '../../businessLogic/todos.mjs'
+
+const logger = createLogger('getTodos')
+
+export async function handler(event) {
+  try {
+    logger.info('Start getting todo tasks')
+
+    const userId = getUserId(event)
+    const todoTasks = await getTodoTasks(userId)
+
+    logger.info('Get todo tasks successfully')
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({ items: todoTasks })
+    }
+  } catch (error) {
+    logger.error(`Get todo tasks failed due to: ${error.message}`)
+
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        status: 'Failed',
+        message: error.message
+      })
+    }
+  }
 }
