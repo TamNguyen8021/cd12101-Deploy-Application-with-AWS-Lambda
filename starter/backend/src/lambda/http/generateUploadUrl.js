f61723f2d05db120db6366d065817008f211ef5b
TamNguyen8021 <tamntt8021@gmail.com>
2024-04-04 22:26:01 +0700

    feat: upload image for todo task

--- starter/backend/src/lambda/http/generateUploadUrl.js
+++ starter/backend/src/lambda/http/generateUploadUrl.js
@@ -1,7 +1,42 @@
-export function handler(event) {
-  const todoId = event.pathParameters.todoId
+import { createLogger } from '../../utils/logger.mjs'
+import { getUserId } from '../utils.mjs'
+import { saveAttachmentUrl } from '../../businessLogic/todos.mjs'
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+const logger = createLogger('generateUploadUrl')
+
+export async function handler(event) {
+  try {
+    logger.info('Start generating url for uploaded file')
+
+    const todoId = event.pathParameters.todoId
+    const userId = getUserId(event)
+    const uploadUrl = await saveAttachmentUrl(userId, todoId)
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        uploadUrl
+      })
+    }
+  } catch (error) {
+    logger.error(
+      `Generate url for uploaded file failed due to: ${error.message}`
+    )
+
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        status: 'Failed',
+        message: error.message
+      })
+    }
+  }
 }
-
