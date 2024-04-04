02537dc44be8fecb3544efdcbf2257cdbd876b3f
TamNguyen8021 <tamntt8021@gmail.com>
2024-04-04 22:23:00 +0700

    feat: create todo task

--- starter/backend/src/lambda/http/createTodo.js
+++ starter/backend/src/lambda/http/createTodo.js
@@ -1,8 +1,42 @@
+import { createLogger } from '../../utils/logger.mjs'
+import { getUserId } from '../utils.mjs'
+import { createTodoTask } from '../../businessLogic/todos.mjs'
 
-export function handler(event) {
-  const newTodo = JSON.parse(event.body)
+const logger = createLogger('createTodo')
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+export async function handler(event) {
+  try {
+    logger.info('Start creating todo task')
+
+    const newTask = JSON.parse(event.body)
+    const userId = getUserId(event)
+    const newTodoTask = await createTodoTask(userId, newTask)
+
+    logger.info(`Create todo task successfully: ${newTodoTask}`)
+
+    return {
+      statusCode: 201,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        item: newTodoTask
+      })
+    }
+  } catch (error) {
+    logger.error(`Create todo task failed due to: ${error.message}`)
+
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        status: 'Failed',
+        message: error.message
+      })
+    }
+  }
 }
-
