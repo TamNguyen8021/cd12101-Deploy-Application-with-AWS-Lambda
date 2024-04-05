2e58bcf875baac3626356b7e1b967f3401201b41
TamNguyen8021 <tamntt8021@gmail.com>
2024-04-05 21:53:58 +0700

    feat: delete todo task

--- starter/backend/src/lambda/http/deleteTodo.js
+++ starter/backend/src/lambda/http/deleteTodo.js
@@ -1,8 +1,41 @@
+import { createLogger } from '../../utils/logger.mjs'
+import { getUserId } from '../utils.mjs'
+import { deleteTodoTask } from '../../businessLogic/todos.mjs'
 
-export function handler(event) {
-  const todoId = event.pathParameters.todoId
+const logger = createLogger('deleteTodo')
 
-  // TODO: Remove a TODO item by id
-  return undefined
+export async function handler(event) {
+  try {
+    logger.info('Start deleting todo task')
+
+    const { todoId } = event.pathParameters
+    const userId = getUserId(event)
+
+    await deleteTodoTask(userId, todoId)
+
+    logger.info(`Delete todo task successfully: ${todoId}`)
+
+    return {
+      statusCode: 204,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: undefined
+    }
+  } catch (error) {
+    logger.error(`Delete todo task failed due to: ${error.message}`)
+
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        status: 'Failed',
+        message: error.message
+      })
+    }
+  }
 }
-
