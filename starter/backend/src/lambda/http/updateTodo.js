c1cd7363f9b1805681cfc531113ffc6ca6b424b7
TamNguyen8021 <tamntt8021@gmail.com>
2024-04-05 21:54:09 +0700

    feat: update todo task

--- starter/backend/src/lambda/http/updateTodo.js
+++ starter/backend/src/lambda/http/updateTodo.js
@@ -1,7 +1,42 @@
-export function handler(event) {
-  const todoId = event.pathParameters.todoId
-  const updatedTodo = JSON.parse(event.body)
-  
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+import { createLogger } from '../../utils/logger.mjs'
+import { getUserId } from '../utils.mjs'
+import { updateTodoTask } from '../../businessLogic/todos.mjs'
+
+const logger = createLogger('updateTodo')
+
+export async function handler(event) {
+  try {
+    logger.info('Start updating todo task')
+
+    const { todoId } = event.pathParameters
+    const updateTask = JSON.parse(event.body)
+    const userId = getUserId(event)
+
+    await updateTodoTask(userId, todoId, updateTask)
+
+    logger.info(`Update todo task successfully: ${todoId}`)
+
+    return {
+      statusCode: 204,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: undefined
+    }
+  } catch (error) {
+    logger.error(`Update todo task failed due to: ${error.message}`)
+
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        status: 'Failed',
+        message: error.message
+      })
+    }
+  }
 }
